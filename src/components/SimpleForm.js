e67852080c9668980d417a66d376b24a1aa5469b
Eduardo Sousa <eduardo91423344@gmail.com>
2024-03-24 12:59:00 -0300

    criacao de regex de email e cpf

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 
 class SimpleForm extends Component {
   render() {
-    const { formData, handleInputChange, handleSubmit } = this.props;
+    const { formData, handleInputChange, handleSubmit,handleOnChangevalidar, cpfMask } = this.props;
 
     return (
       <form onSubmit={handleSubmit}>
@@ -26,7 +26,61 @@ class SimpleForm extends Component {
             onChange={handleInputChange}
           />
         </div>
+        <div>
+          <label htmlFor="telefone">Telefone:</label>
+          <input
+            type="number"
+            id="telefone"
+            name="telefone"
+            value={formData.telefone || ''}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className='endereco'> <h5>Endereço</h5>
+      
+                <div>
+                  <label htmlFor="rua">Rua:</label>
+                  <input
+                    type="text"
+                    id="rua"
+                    name="rua"
+                    value={formData.rua || ''}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="cidade">Cidade:</label>
+                  <input
+                    type="text"
+                    id="cidade"
+                    name="cidade"
+                    value={formData.cidade || ''}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="estado">Estado:</label>
+                  <input
+                    type="text"
+                    id="estado"
+                    name="estado"
+                    value={formData.estado || ''}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="cpf">CPF:</label>
+                  <input
+                    type="text"
+                    id="cpf"
+                    name="cpf"
+                    value={formData.cpf}
+                    onChange={handleInputChange}
+                  />
+                </div>
+      </div>
         <button type="submit">Enviar</button>
+        <button type="button" onClick={handleOnChangevalidar}>testar</button>
       </form>
     );
   }
--- src/components/withFormHandling.js
+++ src/components/withFormHandling.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 
 function withFormHandling(WrappedComponent) {
-  return class FormHOC extends Component {
+  return class  extends Component {
     constructor(props) {
       super(props);
       this.state = {
@@ -18,14 +18,31 @@ function withFormHandling(WrappedComponent) {
           [name]: value,
         },
       }));
-
     };
 
     handleSubmit = (e) => {
       e.preventDefault();
       // Lógica para lidar com o envio do formulário
        console.log(this.state.formData)
+      
     };
+   
+    
+    handleOnChangevalidar =(e)=>{
+      // const email = 'eduardo12@gmail.com'
+      const regEmial = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/i;
+      const email = this.state.formData.email
+      regEmial.test(email)? console.log('email ok'): console.log('email erro')
+
+
+      const regCpf = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;   
+      const cpf = this.state.formData.cpf;
+    
+
+      regCpf.test(cpf)? console.log('cpf ok'): console.log('cpf erro')
+      // console.log(cpfMask.test(cpf))
+
+    }
 
     render() {
       return (
@@ -33,6 +50,8 @@ function withFormHandling(WrappedComponent) {
           formData={this.state.formData}
           handleInputChange={this.handleInputChange}
           handleSubmit={this.handleSubmit}
+          handleOnChangevalidar={this.handleOnChangevalidar}
+          cpfMask={this.cpfMask}
           {...this.props}
         />
       );
